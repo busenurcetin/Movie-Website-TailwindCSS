0ff643ee2284a99e43c96886361b711d174eb074
Buse Nur <110244548+busenurcetin@users.noreply.github.com>
2023-03-24 22:47:53 +0300

    Add files via upload
    Add node_modules to your folder too.

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      screens:{
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px'
+
+      }
+      },
+    extend: {
+      fontFamily:{
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+        },
+
+        colors: {
+
+          'gega-red':' #BC1A45',
+          'gega-melon': '#FFD369',
+          'gega-grey':'#DDDDDD',
+          'gega-white':'F7F7F7',
+          } ,
+
+          spacing: {
+            128: '32rem',
+          },
+    },
+  },
+  plugins: [],
+}
